0a79788c28f16bfcbab38d80885b7738af6bfa9b
d2dileep <41139479+d2dileep@users.noreply.github.com>
2020-08-21 13:28:35 +0530

    latest files

---
+++ trading_RL_yahoo/RL_models/gru-seq2seq-vae.py
@@ -0,0 +1,264 @@
+import sys
+import warnings
+
+if not sys.warnoptions:
+    warnings.simplefilter('ignore')
+
+import tensorflow.compat.v1 as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from datetime import datetime
+from datetime import timedelta
+from tqdm import tqdm
+sns.set()
+tf.disable_eager_execution()
+tf.disable_v2_behavior()
+
+
+file = sys.argv[1]
+df = pd.read_csv(file)
+print(df.head())
+
+
+minmax = MinMaxScaler().fit(df.iloc[:, 4:5].astype('float32')) # Close index
+df_log = minmax.transform(df.iloc[:, 4:5].astype('float32')) # Close index
+df_log = pd.DataFrame(df_log)
+df_log.head()
+
+
+
+
+test_size = 5
+prediction_size = 5
+
+df_train = df_log.iloc[:-test_size]
+df_test = df_log.iloc[-test_size:]
+df.shape, df_train.shape, df_test.shape
+
+
+class Model:
+    def __init__(
+            self,
+            learning_rate,
+            num_layers,
+            size,
+            size_layer,
+            output_size,
+            forget_bias=0.1,
+            lambda_coeff=0.5
+    ):
+        def lstm_cell(size_layer):
+            return tf.nn.rnn_cell.GRUCell(size_layer)
+
+        rnn_cells = tf.nn.rnn_cell.MultiRNNCell(
+            [lstm_cell(size_layer) for _ in range(num_layers)],
+            state_is_tuple=False,
+        )
+        self.X = tf.placeholder(tf.float32, (None, None, size))
+        self.Y = tf.placeholder(tf.float32, (None, output_size))
+        drop = tf.nn.rnn_cell.DropoutWrapper(
+            rnn_cells, output_keep_prob=forget_bias
+        )
+        self.hidden_layer = tf.placeholder(
+            tf.float32, (None, num_layers * size_layer)
+        )
+        _, last_state = tf.nn.dynamic_rnn(
+            drop, self.X, initial_state=self.hidden_layer, dtype=tf.float32
+        )
+
+        self.z_mean = tf.layers.dense(last_state, size)
+        self.z_log_sigma = tf.layers.dense(last_state, size)
+
+        epsilon = tf.random_normal(tf.shape(self.z_log_sigma))
+        self.z_vector = self.z_mean + tf.exp(self.z_log_sigma)
+
+        with tf.variable_scope('decoder', reuse=False):
+            rnn_cells_dec = tf.nn.rnn_cell.MultiRNNCell(
+                [lstm_cell(size_layer) for _ in range(num_layers)], state_is_tuple=False
+            )
+            drop_dec = tf.nn.rnn_cell.DropoutWrapper(
+                rnn_cells_dec, output_keep_prob=forget_bias
+            )
+            x = tf.concat([tf.expand_dims(self.z_vector, axis=0), self.X], axis=1)
+            self.outputs, self.last_state = tf.nn.dynamic_rnn(
+                drop_dec, self.X, initial_state=last_state, dtype=tf.float32
+            )
+
+        self.logits = tf.layers.dense(self.outputs[-1], output_size)
+        self.lambda_coeff = lambda_coeff
+
+        self.kl_loss = -0.5 * tf.reduce_sum(1.0 + 2 * self.z_log_sigma - self.z_mean ** 2 -
+                                            tf.exp(2 * self.z_log_sigma), 1)
+        self.kl_loss = tf.scalar_mul(self.lambda_coeff, self.kl_loss)
+        self.cost = tf.reduce_mean(tf.square(self.Y - self.logits) + self.kl_loss)
+        self.optimizer = tf.train.AdamOptimizer(learning_rate).minimize(
+            self.cost
+        )
+
+def calculate_accuracy(real, predict):
+    real = np.array(real) + 1
+    predict = np.array(predict) + 1
+    percentage = 1 - np.sqrt(np.mean(np.square((real - predict) / real)))
+    return percentage * 100
+
+def anchor(signal, weight):
+    buffer = []
+    last = signal[0]
+    for i in signal:
+        smoothed_val = last * weight + (1 - weight) * i
+        buffer.append(smoothed_val)
+        last = smoothed_val
+    return buffer
+
+
+num_layers = 1
+size_layer = 128
+timestamp = 5
+epoch = 10
+dropout_rate = 0.8
+future_day = test_size
+learning_rate = 0.01
+
+
+def forecast():
+    tf.reset_default_graph()
+    modelnn = Model(
+        learning_rate, num_layers, df_log.shape[1], size_layer, df_log.shape[1], dropout_rate
+    )
+    sess = tf.InteractiveSession()
+    sess.run(tf.global_variables_initializer())
+    date_ori = pd.to_datetime(df.iloc[:, 0]).tolist()
+
+    pbar = tqdm(range(epoch), desc='train loop')
+    for i in pbar:
+        init_value = np.zeros((1, num_layers * size_layer))
+        total_loss, total_acc = [], []
+        for k in range(0, df_train.shape[0] - 1, timestamp):
+            index = min(k + timestamp, df_train.shape[0] - 1)
+            batch_x = np.expand_dims(
+                df_train.iloc[k: index, :].values, axis=0
+            )
+            batch_x = np.random.binomial(1, 0.5, batch_x.shape) * batch_x
+            batch_y = df_train.iloc[k + 1: index + 1, :].values
+            logits, last_state, _, loss = sess.run(
+                [modelnn.logits, modelnn.last_state, modelnn.optimizer, modelnn.cost],
+                feed_dict={
+                    modelnn.X: batch_x,
+                    modelnn.Y: batch_y,
+                    modelnn.hidden_layer: init_value,
+                },
+            )
+            init_value = last_state
+            total_loss.append(loss)
+            total_acc.append(calculate_accuracy(batch_y[:, 0], logits[:, 0]))
+        pbar.set_postfix(cost=np.mean(total_loss), acc=np.mean(total_acc))
+
+    future_day = test_size
+
+    output_predict = np.zeros((df_train.shape[0] + future_day, df_train.shape[1]))
+    output_predict[0] = df_train.iloc[0]
+    upper_b = (df_train.shape[0] // timestamp) * timestamp
+    init_value = np.zeros((1, num_layers * size_layer))
+
+    for k in range(0, (df_train.shape[0] // timestamp) * timestamp, timestamp):
+        out_logits, last_state = sess.run(
+            [modelnn.logits, modelnn.last_state],
+            feed_dict={
+                modelnn.X: np.expand_dims(
+                    df_train.iloc[k: k + timestamp], axis=0
+                ),
+                modelnn.hidden_layer: init_value,
+            },
+        )
+        init_value = last_state
+        output_predict[k + 1: k + timestamp + 1] = out_logits
+
+    if upper_b != df_train.shape[0]:
+        out_logits, last_state = sess.run(
+            [modelnn.logits, modelnn.last_state],
+            feed_dict={
+                modelnn.X: np.expand_dims(df_train.iloc[upper_b:], axis=0),
+                modelnn.hidden_layer: init_value,
+            },
+        )
+        output_predict[upper_b + 1: df_train.shape[0] + 1] = out_logits
+        future_day -= 1
+        date_ori.append(date_ori[-1] + timedelta(days=test_size+1))
+
+    init_value = last_state
+
+    for i in range(future_day):
+        o = output_predict[-future_day - timestamp + i:-future_day + i]
+        out_logits, last_state = sess.run(
+            [modelnn.logits, modelnn.last_state],
+            feed_dict={
+                modelnn.X: np.expand_dims(o, axis=0),
+                modelnn.hidden_layer: init_value,
+            },
+        )
+        init_value = last_state
+        output_predict[-future_day + i] = out_logits[-1]
+        date_ori.append(date_ori[-1] + timedelta(days=1))
+
+    output_predict = minmax.inverse_transform(output_predict)
+    deep_future = anchor(output_predict[:, 0], 0.3)
+
+    return deep_future[-test_size:]
+
+results = []
+sim = []
+
+for i in range(prediction_size):
+    print('prediction %d'%(i + 1))
+    sim.append('prediction' + str(i + 1))
+    results.append(forecast())
+
+sim.append('Current/Real Values')
+cl = df.Close.values.tolist()
+cl1 = cl[-test_size:]
+results.append(cl1)
+d = pd.DataFrame(results)
+d.index = sim
+date1 = df.Date.values.tolist()
+date3=[]
+for date in date1:
+    if(str(date)!="nan"):
+        date3.append(datetime.strptime(str(date), "%m/%d/%Y")+timedelta(days=30))
+    
+    #print(date)
+date2 = date1[-test_size:]
+d.columns = date2
+d.columns = date3[-test_size:]
+x = d.loc['Current/Real Values'].array
+x1 = d.loc['prediction1'].array
+x2 = d.loc['prediction2'].array
+x3 = d.loc['prediction3'].array
+x4 = d.loc['prediction4'].array
+x5 = d.loc['prediction5'].array
+z = []
+z.append(x1)
+z.append(x2)
+z.append(x3)
+z.append(x4)
+z.append(x5)
+
+
+diff = []
+for j in range(prediction_size):
+    t=0
+    for i in range(test_size):
+        t += abs(x[i]-z[j][i])
+    diff.append(t)
+ans = "prediction"+str(diff.index(min(diff))+1)
+xans = d.loc[ans]
+yans = d.loc['Current/Real Values']
+dffinal = pd.concat([yans, xans], axis=1)
+print(dffinal.tail(2))
+dffinal.to_csv("gru-seq2seq-vae.csv")
+
+
+
+
